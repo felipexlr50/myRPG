3ae9b7a6ef57acc14a5bae7ec01040fe7897f1f4
ezbapfe <felipe.baptista@ericsson.com>
2018-01-22 09:33:58 -0200

    config static files

--- webserver.py
+++ webserver.py
@@ -1,27 +1,42 @@
-from bottle import route, run, get, post, request, error, response, default_app, template, debug, static_file, Bottle
+#from bottle import route, run, get, post, request, error, response, default_app, template, debug, static_file, Bottle
 import json
 import sessioncontroller as controller
 import os
 import sys
+import bottle
+
 
 dirname = os.path.dirname(sys.argv[0])
 
-app = Bottle()
+app = bottle.Bottle()
+
+bottle.TEMPLATE_PATH.insert(0, 'view')
 
 
-@app.route('/static/<filename:re:.*\.css>')
-def send_css(filename):
-    return static_file(filename, root=dirname + '/static/asset/css')
+# Static Routes
+@app.route("/static/css/<filepath:re:.*\.css>")
+def css(filepath):
+    return bottle.static_file(filepath, root="static/css")
 
 
-@app.route('/static/<filename:re:.*\.js>')
-def send_js(filename):
-    return static_file(filename, root=dirname + '/static/asset/js')
+@app.route("/static/font/<filepath:re:.*\.(eot|otf|svg|ttf|woff|woff2?)>")
+def font(filepath):
+    return bottle.static_file(filepath, root="static/font")
+
+
+@app.route("/static/img/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
+def img(filepath):
+    return bottle.static_file(filepath, root="static/img")
+
+
+@app.route("/static/js/<filepath:re:.*\.js>")
+def js(filepath):
+    return bottle.static_file(filepath, root="static/js")
 
 
 @app.route('/')
 def index():
-    return template('index')
+    return bottle.template('index')
 
 
 @app.route('/teste')
@@ -29,4 +44,4 @@ def sensors():
     return controller.main()
 
 
-app.run(host='0.0.0.0', port=8080, debug=True, server='paste', reloader=True)
+#app.run(host='0.0.0.0', port=8080, debug=True, reloader=False)
--- webserver_test.py
+++ webserver_test.py
@@ -1,10 +1,47 @@
-from bottle import route, run, get, post, request, error, response
+#from bottle import route, run, get, post, request, error, response, default_app, template, debug, static_file, Bottle
 import json
+import sessioncontroller as controller
+import os
+import sys
+import bottle
 
 
-@route('/teste')
+dirname = os.path.dirname(sys.argv[0])
+
+app = bottle.Bottle()
+
+bottle.TEMPLATE_PATH.insert(0, 'view')
+
+
+# Static Routes
+@app.route("/static/css/<filepath:re:.*\.css>")
+def css(filepath):
+    return bottle.static_file(filepath, root="static/css")
+
+
+@app.route("/static/font/<filepath:re:.*\.(eot|otf|svg|ttf|woff|woff2?)>")
+def font(filepath):
+    return bottle.static_file(filepath, root="static/font")
+
+
+@app.route("/static/img/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
+def img(filepath):
+    return bottle.static_file(filepath, root="static/img")
+
+
+@app.route("/static/js/<filepath:re:.*\.js>")
+def js(filepath):
+    return bottle.static_file(filepath, root="static/js")
+
+
+@app.route('/')
+def index():
+    return bottle.template('index')
+
+
+@app.route('/teste')
 def sensors():
-    return "teste"
+    return controller.main()
 
 
-run(host='0.0.0.0', port=8080, debug=True, server='paste', reloader=True)
+app.run(host='0.0.0.0', port=8080, debug=True, reloader=False)
